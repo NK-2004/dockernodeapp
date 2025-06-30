bbdfb26763b11982883e84669dc4755f20ca66c9
nitin <npkobarne@gmail.com>
2025-06-30 13:49:56 +0000

    Docker-build

---
+++ index.js
@@ -0,0 +1,14 @@
+// index.js
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+const server = app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
+
+module.exports = { app, server };
+
